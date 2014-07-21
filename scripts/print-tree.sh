1f468978af1e1eb8f334cea94ce858a6081b7149
Tim Camper <twcamper@thoughtworks.com>
2014-07-21 17:41:42 -0500

    update exercises

--- scripts/print-tree.sh
+++ scripts/print-tree.sh
@@ -1,35 +1,12 @@
 #! /usr/bin/env bash
 
-print-tree-recursive()
-{
-  local dir=$1
-  local depth_indent=$2
-
-  echo ${depth_indent}+$(basename $dir)
-
-  depth_indent+='  '
-  for entry in $(ls $dir)
-  do
-    if [ -d $dir/$entry ]  # directory?
-    then
-      print-tree-recursive $dir/$entry $depth_indent
-    else
-      echo $depth_indent$entry
-    fi
-  done
-}
-
-IFS=$'\n' # only split on newlines so 'ls' call doesn't break up names with spaces
-
-print-tree-recursive $(pwd)
-
 ###################################################################################
 # Exercises
 ###################################################################################
 
 # 1 - Make the script start with a directory supplied as an argument from the command line, rather than current working directory
 # 2 - What happens if you don't use the 'basename' command?  What happens if $dir or $depth_indent aren't local?
-# 3 - Refactor the script so it gives identical output, but works using cd rather than ls.  That is, you won't give ls a path when you
+# 3 - Refactor the script so it gives identical output, but works using 'cd' rather than 'ls'.  That is, you won't give 'ls' a path when you
 #     call it in the 'for' loop, and the 'for' loop will only use 2 variables instead of three.
 # 4   Does one version run faster?  Use the 'time' command and point to a big directory.
 # 5 - Instead of printing entries in each dir (the dir and file names), just print the dir name and its depth in the tree as a number.
@@ -46,3 +23,27 @@ print-tree-recursive $(pwd)
 #           tavern 3
 #             grill 4
 #               kitchen 5
+# 6 - Add an option to 'ls' that will make the script walk UP the tree and print your entire file system
+
+print-tree-recursive()
+{
+  local dir=$1
+  local depth_indent=$2
+
+  echo ${depth_indent}+$(basename $dir)
+
+  depth_indent+='  '
+  for entry in $(ls $dir)
+  do
+    if [ -d $dir/$entry ]  # directory?
+    then
+      print-tree-recursive $dir/$entry $depth_indent
+    #else
+      #echo $depth_indent$entry
+    fi
+  done
+}
+
+IFS=$'\n' # only split on newlines so 'ls' call doesn't break up names with spaces
+
+print-tree-recursive $(pwd)
